56c9c7bfe724090a219330e53b6341ee4974b366
elenamanolache65 <elenamanolache53@gmail.com>
2024-10-20 17:34:20 +0300

    finish

---
+++ js/task-02.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const arrayOfItems = ingredients.map((element) => {
+  const listOfItems = document.createElement("li");
+  listOfItems.textContent = element;
+  listOfItems.classList.add("item");
+  // console.log(listOfItems);
+  return listOfItems;
+});
+
+const listEl = document.querySelector("#ingredients");
+listEl.append(...arrayOfItems);
